073e9543ac369404aa0ebde5ab6b5d219c8f0964
Petter Lindgren <petlindg@student.chalmers.se>
2024-12-10 12:11:27 +0100

    day 10 solutions

---
+++ 10/q2.py
@@ -0,0 +1,38 @@
+def inBounds(room, x, y):
+    h = len(room)
+    w = len(room[0])
+    return 0 <= x and x < h and 0 <= y and y < w
+
+def path (room, x, y):
+    dsts = []
+    dirs = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+    e1 = room[x][y]
+    for (i, j) in dirs:
+        if inBounds(room, i, j):
+            e2 = room[i][j] 
+            if e2==e1+1:
+                if e2 == 9:
+                    dsts.append((i, j))
+                else:
+                    dsts += path(room, i, j)
+    return dsts
+
+def score(room):
+    h = len(room)
+    w = len(room[0])
+    r = 0
+    for x in range(h):
+        for y in range(w):
+            if room[x][y] == 0:
+                
+                r += len(path(room, x, y))
+    return r
+
+
+
+room = [list(map(int, line)) for line in open("input.txt", "r").read().split('\n')]
+
+r = score(room)
+
+print(r)
+
